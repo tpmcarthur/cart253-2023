e8850d739fcec726d5f22337023aab564e75bb8c
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-09-19 13:58:27 -0400

    Finished learning about js objects

--- experiments/drawing-experiments w2/js/script.js
+++ experiments/drawing-experiments w2/js/script.js
@@ -2,7 +2,7 @@
 Exercise 1: I like to move it!
 Taylor McArthur
 
-Learning about changing variables, math
+Learning about Javascript Objects
 ***********************/
 
 "use strict";
@@ -16,11 +16,13 @@ function preload() {
 
 //Declaring variables
 let backgroundShade = 0;
-let circleX = 0;
-let circleY = 250;
-let circleSize = 200;
-let circleSpeed = 2;
-let circleAcceleration = 0.25;
+
+let circle = {
+  x: 0,
+  y: 250,
+  size: 200,
+  speed: 2
+}; 
 
 function setup(){
   createCanvas(500,500);
@@ -29,8 +31,6 @@ function setup(){
 //Draw mouse shape
 function draw(){
   background(backgroundShade);
-  circleX +=circleSpeed;
-  circleSpeed += circleAcceleration;
-  ellipse(circleX,circleY,circleSize);
-
+  circle.x +=circle.speed;
+  ellipse(circle.x,circle.y,circle.size);
 }
