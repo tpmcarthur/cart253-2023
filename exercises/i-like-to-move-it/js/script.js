923e2d07efbc6457e112b08b1a1bb37402ef3adb
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-09-19 18:25:53 -0400

    E1: Started exercise 1

---
+++ exercises/i-like-to-move-it/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * i-like-to-move-it
+ * Taylor McArthur
+ * 
+ * Complete exercise to show all that we have learned over the past couple of weeks using variables
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
