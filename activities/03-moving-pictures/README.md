5fd798388f3e298d2997ebd31943b1685167ee90
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-09-19 17:00:18 -0400

    Activity 03 Moving Pictures
    Start on activity 03 moving pictures after learning more in-depth about variables

---
+++ activities/03-moving-pictures/README.md
@@ -0,0 +1,7 @@
+# Activity 03, moving pictures
+
+This is the README file for the entire project. 
+
+Putting to use variables, introducing, creating, changing, intro to JS objects, debugginh, random numbers, map and constrain into Activity 03
+
+Taylor McArthur CART 253 B
---
+++ activities/03-moving-pictures/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Activity 03 - Moving Pictures
+ * Taylor McArthur
+ * 
+ * Completeing activity 3 to put to use everything learned about variables, more info found in README
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
