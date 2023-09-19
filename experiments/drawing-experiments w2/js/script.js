58e9b751d2d1135bb73bf1a9ad82a3bc5240219b
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-09-19 14:44:18 -0400

    learned about debugging variables

--- experiments/drawing-experiments w2/js/script.js
+++ experiments/drawing-experiments w2/js/script.js
@@ -2,7 +2,7 @@
 Exercise 1: I like to move it!
 Taylor McArthur
 
-Learning about Javascript Objects
+Debugging variables
 ***********************/
 
 "use strict";
@@ -33,4 +33,6 @@ function draw(){
   background(backgroundShade);
   circle.x +=circle.speed;
   ellipse(circle.x,circle.y,circle.size);
+
+  // console.log(`circle.x: ${circle.x}, circle.y: ${circle.y}, circle.size: ${circle.size},`);
 }
