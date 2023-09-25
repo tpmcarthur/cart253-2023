2ca289403e93f1fe0b45d46552655273b1823a54
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-09-25 19:33:16 -0400

    Completed learning if else statements

--- activities/conditionals/js/script.js
+++ activities/conditionals/js/script.js
@@ -7,6 +7,13 @@
  */
 
 "use strict";
+let backgroundShade = 0;
+let circle = {
+    x:0,
+    y:250,
+    size: 100,
+    speed: 5
+}
 
 /**
  * Description of preload
@@ -15,18 +22,39 @@ function preload() {
 
 }
 
-
 /**
  * Description of setup
 */
 function setup() {
-
+    createCanvas(500,500);
 }
-
 
 /**
  * Description of draw()
 */
 function draw() {
+    background(backgroundShade);
 
+    circle.x = circle.x + circle.speed;
+
+    if (circle.x > width){
+        circle.speed = -circle.speed;
+    }
+
+    if (circle.x <0){
+        circle.speed = -circle.speed;
+    }
+
+    //Mouse color red
+    if (mouseY < height/2){
+        fill(255,0,0);
+    }
+
+    //Mouse color blue
+    if (mouseY > height/2){
+        fill(0,0,255);
+    }
+
+    ellipse(circle.x,circle.y,circle.size);
 }
+
