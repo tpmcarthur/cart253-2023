2a503ab66540905ef3e2ecaf63e55c1aee7ffab0
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-09-26 10:38:53 -0400

    Learning mouse inputs

--- activities/conditionals/js/script.js
+++ activities/conditionals/js/script.js
@@ -1,13 +1,18 @@
 /**
- * Loops 4.5
+ * Loops 4.6
  * Taylor McArthur
  * 
  *  
- * Loops beyond counting
+ * Mouse input
  */
 
 "use strict";
 
+let circle = {
+    x: 250,
+    y: 250,
+    size: 100
+}
 
 /**
  * Description of preload
@@ -16,34 +21,11 @@ function preload() {
 
 }
 
-let circle = {
-    x: undefined,
-    y: undefined,
-    size: 100
-    
-};
-
-let dangerZone = {
-    x: 250,
-    y: 250,
-    size: 150
-}
-
 /**
  * Description of setup
 */
 function setup() {
  createCanvas(500,500);   
-
- circle.x = random(0,width);
- circle.y = random(0,height);
-
- let d = dist(circle.x,circle.y,dangerZone.x,dangerZone.y);
- if (d <circle.size/2 + dangerZone.size/2){
-    circle.x = random(0,width);
-    circle.y = random(0,height);
-
- }
 
 }
 
@@ -53,13 +35,6 @@ function setup() {
 function draw() {
    background(0);
 
-    //Highway to the dangerzone
-    noFill();
-    stroke(255,0,0);
-    ellipse(dangerZone.x,dangerZone.y,dangerZone.size);
-   
-   fill(255);
-   noStroke();
    ellipse(circle.x,circle.y,circle.size);
 }
 
