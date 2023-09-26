9c27d8ba09c843f41143b2d33cd127f0e48502a3
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-09-26 15:13:07 -0400

    Relearning Movement

--- activities/conditionals/js/script.js
+++ activities/conditionals/js/script.js
@@ -1,26 +1,12 @@
 /**
- * Mouse Input 4.6
+ * Mouse Input 4.7
  * Taylor McArthur
  * 
  *  
- * Mouse input
+ * Learning Movement
  */
 
 "use strict";
-
-// Our circle object
-let circle = {
-    x: 250,
-    y: 250,
-    size: 50,
-    // Because it changes size, let's set a minimum and maximum size
-    minSize: 50,
-    maxSize: 400,
-    fill: 0,
-    // We need to keep track of whether the circle is being dragged or not
-    // so we know whether to move it with the mouse position
-    dragging: false
-  }
 
 /**
  * Description of preload
@@ -41,32 +27,5 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    background(127);
-    fill(circle.fill);
-    ellipse(circle.x, circle.y, circle.size);
+
   }
-
-function mousePressed(){
-    let d = dist(mouseX,mouseY,circle.x,circle.y);
-
-    if (d < circle.size / 2){
-        circle.dragging = true;
-    }
-}
-
-function mouseReleased(){
-    circle.dragging = false;
-}
-
-function mouseDragged(){
-    
-    if (circle.dragging){
-        circle.x = mouseX;
-        circle.y = mouseY;
-    }
-}
-
-function mouseWheel(event){
-    circle.size += event.delta;
-    circle.size = constrain(circle.size,circle.minSize,circle.maxSize);
-}
