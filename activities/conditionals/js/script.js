5e2bdb69e0ccc2b150f5d5546a1a5d2ace2216cd
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-09-26 09:38:24 -0400

    Update script.js

--- activities/conditionals/js/script.js
+++ activities/conditionals/js/script.js
@@ -1,9 +1,9 @@
 /**
- * Booleans 4.3
+ * Loops 4.4
  * Taylor McArthur
  * 
  *  
- * Learning Booleans
+ * Loops exercise, learning how to use loops for effieceny 
  */
 
 "use strict";
@@ -16,7 +16,11 @@ function preload() {
 
 }
 
-let displayCircle = false;
+let caterpillar = {
+    x: 100,
+    y: 250,
+    segmentSize: 50
+}
 
 /**
  * Description of setup
@@ -29,15 +33,26 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-
     background(0);
+    noStroke();
+    fill(100,200,100);
 
-    if(mouseIsPressed){
-        displayCircle = true;
-    }
+    // let x = caterpillar.x;
+    // let numSegments = 5;
+    // let segmentsDrawn = 0;
 
-    if(displayCircle){
-        ellipse(250,250,100,100);
+    // while(segmentsDrawn < numSegments){
+    //     ellipse(x,caterpillar.y,caterpillar.segmentSize); 
+    //     x = x + 40;  
+    //     segmentsDrawn++;
+    // }
+
+    let x = caterpillar.x;
+    let numSegments = 10;
+
+    for(let segmentsDrawn = 0; segmentsDrawn < numSegments; segmentsDrawn++){
+      ellipse(x,caterpillar.y,caterpillar.segmentSize);
+      x = x + 40;
     }
 }
 
