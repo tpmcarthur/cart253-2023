22d8ad8db1262ce75bbf2a7f9ddf0a87c06fc05c
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-09-26 15:10:42 -0400

    Draggable, scaleable circle

--- activities/conditionals/js/script.js
+++ activities/conditionals/js/script.js
@@ -8,6 +8,20 @@
 
 "use strict";
 
+// Our circle object
+let circle = {
+    x: 250,
+    y: 250,
+    size: 50,
+    // Because it changes size, let's set a minimum and maximum size
+    minSize: 50,
+    maxSize: 400,
+    fill: 0,
+    // We need to keep track of whether the circle is being dragged or not
+    // so we know whether to move it with the mouse position
+    dragging: false
+  }
+
 /**
  * Description of preload
 */
@@ -27,9 +41,32 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    if (mouseIsPressed){
-        line(pmouseX,pmouseY,mouseX,mouseY)
-    }
+    background(127);
+    fill(circle.fill);
+    ellipse(circle.x, circle.y, circle.size);
+  }
 
+function mousePressed(){
+    let d = dist(mouseX,mouseY,circle.x,circle.y);
+
+    if (d < circle.size / 2){
+        circle.dragging = true;
+    }
 }
 
+function mouseReleased(){
+    circle.dragging = false;
+}
+
+function mouseDragged(){
+    
+    if (circle.dragging){
+        circle.x = mouseX;
+        circle.y = mouseY;
+    }
+}
+
+function mouseWheel(event){
+    circle.size += event.delta;
+    circle.size = constrain(circle.size,circle.minSize,circle.maxSize);
+}
