bce661a8454ce32357ecb368b3a9a92393e79828
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-09-26 10:23:14 -0400

    4.5, Loops beyond counting

--- activities/conditionals/js/script.js
+++ activities/conditionals/js/script.js
@@ -1,9 +1,9 @@
 /**
- * Loops 4.4
+ * Loops 4.5
  * Taylor McArthur
  * 
  *  
- * Loops exercise, learning how to use loops for effieceny 
+ * Loops beyond counting
  */
 
 "use strict";
@@ -16,10 +16,17 @@ function preload() {
 
 }
 
-let caterpillar = {
-    x: 100,
+let circle = {
+    x: undefined,
+    y: undefined,
+    size: 100
+    
+};
+
+let dangerZone = {
+    x: 250,
     y: 250,
-    segmentSize: 50
+    size: 150
 }
 
 /**
@@ -27,32 +34,32 @@ let caterpillar = {
 */
 function setup() {
  createCanvas(500,500);   
+
+ circle.x = random(0,width);
+ circle.y = random(0,height);
+
+ let d = dist(circle.x,circle.y,dangerZone.x,dangerZone.y);
+ if (d <circle.size/2 + dangerZone.size/2){
+    circle.x = random(0,width);
+    circle.y = random(0,height);
+
+ }
+
 }
 
 /**
  * Description of draw()
 */
 function draw() {
-    background(0);
-    noStroke();
-    fill(100,200,100);
+   background(0);
 
-    // let x = caterpillar.x;
-    // let numSegments = 5;
-    // let segmentsDrawn = 0;
-
-    // while(segmentsDrawn < numSegments){
-    //     ellipse(x,caterpillar.y,caterpillar.segmentSize); 
-    //     x = x + 40;  
-    //     segmentsDrawn++;
-    // }
-
-    let x = caterpillar.x;
-    let numSegments = 10;
-
-    for(let segmentsDrawn = 0; segmentsDrawn < numSegments; segmentsDrawn++){
-      ellipse(x,caterpillar.y,caterpillar.segmentSize);
-      x = x + 40;
-    }
+    //Highway to the dangerzone
+    noFill();
+    stroke(255,0,0);
+    ellipse(dangerZone.x,dangerZone.y,dangerZone.size);
+   
+   fill(255);
+   noStroke();
+   ellipse(circle.x,circle.y,circle.size);
 }
 
