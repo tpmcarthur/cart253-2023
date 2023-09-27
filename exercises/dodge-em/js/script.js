326b4ecd4845024427b2f19552d4f8215726859c
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-09-26 21:29:32 -0400

    Customized
    Added images, added new if else statement, changed colours, added movement, used constrain to increase and decrease size of black hole

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -2,33 +2,30 @@
  * dodge-em
  * Taylor McArthur
  * 
- * Exercise, dodge em 
+ * Exercise, dodge em, black hole 
  */
 
 "use strict";
-let starImage;
 
 //Object variable aka "circle"
-let covid19 = {
+let blackhole = {
     x: 0,
     y: 250,
     size: 100,
-    vx: 0, //moves with mouse
-    vy: 0, //moves with mouse
+    // vx: 0, //moves with mouse
+    // vy: 0, //moves with mouse
     speed: 5,
-    fill:{
-        r: 255,
-        g: 0,
-        b: 0
-    }
+    fill: 255,
+    image: undefined,
 };
 
-// let player = {
-//     x: 250,
-//     y: 250,
-//     size: 100,
-//     fill: 255
-// };
+let player = {
+    x: 250,
+    y: 250,
+    size: 100,
+    fill: 255,
+    image: undefined,
+};
 
 let numStatic = 100;
 
@@ -36,7 +33,6 @@ let numStatic = 100;
  * Description of preload
 */
 function preload() {
-    starImage = loadImage("assets/images/star.png")
 
 }
 
@@ -46,8 +42,14 @@ function preload() {
 function setup() {
     createCanvas(windowWidth,windowHeight);
 
-    covid19.y = random(0,height);
-    covid19.vx = covid19.speed;
+    imageMode(CENTER);
+    player.image = loadImage("assets/images/star.png")
+
+    blackhole.image = loadImage("assets/images/blackhole.png")
+
+    blackhole.y = random(0,height);
+    blackhole.vx = blackhole.speed;
+
 }
 
 /**
@@ -55,6 +57,7 @@ function setup() {
 */
 function draw() {
     background(20);
+    noCursor();
 
     //Static background
     push();
@@ -66,52 +69,59 @@ function draw() {
         point(x,y);
     }
     pop();
-
-    Image(starImage,mouseX,mouseY);
-
-    //Mouse POS greater than covid19 x pos, move to right
-    if(mouseX > covid19.x){
-        //covid19 velocity X positive num, move to right
-        covid19.vx = 1;
+    
+    //Mouse POS greater than blackhole x pos, move to right
+    if(mouseX > blackhole.x){
+        //blackhole velocity X positive num, move to right
+        blackhole.vx = 1;
     }
 
-    //Or if mouse POS X is less than covid19 X pos, move to left
-    else if (mouseX < covid19.x){
+    //Or if mouse POS X is less than blackhole X pos, move to left
+    else if (mouseX < blackhole.x){
         //set velocity of covid 19 to neg num, move left 
-        covid19.vx = -1;
+        blackhole.vx = -1;
     }
 
-    //Mouse POS greater than covid19 y pos, move below 
-    if (mouseY > covid19.y){
-        //set velocity of covid19 x to positive num, move down
-        covid19.vy = 1;
+    //Covid 19 reaching right half of screen, size increases
+    if(blackhole.x > width/2){
+        blackhole.size = blackhole.size + 1;
     }
 
-    //Mouse POS less than covid 19 y pos, move above
-    else if (mouseY <covid19.y){
-        //set velocity of covid19 to negative num, move up
-        covid19.vy = -1;
+    // when going back to left side reduce size, stop at 100
+    else if(blackhole.size = blackhole.size -1)
+        blackhole.size = constrain(blackhole.size,100,width);
+
+    //Mouse POS greater than blackhole y pos, move below 
+    if (mouseY > blackhole.y){
+        //set velocity of blackhole x to positive num, move down
+        blackhole.vy = 1;
     }
 
-    //Apply the changes to vx and vy to covid 19 position
-    covid19.x = covid19.x + covid19.vx;
-    covid19.y = covid19.y + covid19.vy;
+    //Mouse POS less than blackhole y pos, move above
+    else if (mouseY <blackhole.y){
+        //set velocity of blackhole to negative num, move up
+        blackhole.vy = -1;
+    }
+
+    //Apply the changes to vx and vy to blackhole position
+    blackhole.x = blackhole.x + blackhole.vx;
+    blackhole.y = blackhole.y + blackhole.vy;
 
     //player movement
-    starImage.x = mouseX;
-    starImage.y = mouseY;
+    player.x = mouseX;
+    player.y = mouseY;
 
-    //check for catching covid
-    let d = dist(starImage.x,starImage.y,covid19.x,covid19.y);
-    if (d < covid19.size/2 + starImage.size/2){
+    //check for blackhole
+    let d = dist(player.x,player.y,blackhole.x,blackhole.y);
+    if (d < blackhole.size/2 + player.size/2){
         noLoop();
     }
 
-    //display covid19
-    fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
-    ellipse(covid19.x,covid19.y,covid19.size);
+    //display blackhole
+    fill(blackhole.fill);
+    image(blackhole.image,blackhole.x,blackhole.y,blackhole.size,blackhole.size);
 
     //display player
-    fill(starImage.fill);
-    ellipse(starImage.x,starImage.y,starImage.size);
+    fill(player.fill);
+    image(player.image,player.x,player.y,player.size, player.size);
 }
