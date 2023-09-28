39604a32f6e5b1b025f7b4cf2ccd9fbfa55575ae
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-09-28 17:42:10 -0400

    Update script.js

--- activities/05-functions/js/script.js
+++ activities/05-functions/js/script.js
@@ -1,20 +1,11 @@
 /**
- * Functions
+ * Functions with parameters
  * Taylor McArthur
  * 
- * Learning functions 5.1
+ * Learning functions 5.2
  */
 
 "use strict";
-
-let circle = {
-    x: 0,
-    y: 250,
-    size: 100,
-    vx: 1,
-    vy: 0
-
-};
 
 //Preload 
 function preload() {
@@ -32,43 +23,7 @@ function setup() {
 function draw() {
     background(0);
 
-    move();
-    wrap();
-    display();
-
 }
 
-//moves the circle
-function move() {
-    
-    circle.x = circle.x + circle.vx;
-    circle.y = circle.y + circle.vy;
-}
 
-//circle grows larger and moves faster 
-function wrap(){
-    if (circle.x > width) {
-        reset();
-    }
-}
 
-//draws circle
-function display(){
-    fill(255,0,0);
-    ellipse(circle.x,circle.y,circle.size);
-
-}
-
-//Creating a new function
-function reset(){
-    circle.x = 0;
-    circle.vx = circle.vx + 2;
-    circle.vy = circle.vy - 0.25;
-    circle.size = circle.size + 5;
-
-} 
-
-//same happens as above if clicked with mouse
-function mousePressed() {
-    reset();
-}
