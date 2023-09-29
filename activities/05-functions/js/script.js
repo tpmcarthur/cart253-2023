96801df2cc6ec3c0789bbd6461ae8666f75618fd
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-09-29 15:13:54 -0400

    different ways to write strings

--- activities/05-functions/js/script.js
+++ activities/05-functions/js/script.js
@@ -5,6 +5,10 @@
  * text 5.4
  */
 
+let hamlet = "To be or not to be"; // string of text
+let hamlet = 'That is the question'; //another way to write it out
+let hamlet3 = `Whether 'tis nobler in the mind...`; // all do the same, just different ways to indicate a string is being written/stored
+
 "use strict";
 
 //Preload 
