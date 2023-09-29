7d5e366f7da89e65357a1ca4dd421912d38b1c39
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-09-29 11:48:33 -0400

    Update script.js

--- activities/05-functions/js/script.js
+++ activities/05-functions/js/script.js
@@ -23,6 +23,19 @@ function setup() {
 function draw() {
     background(0);
 
+    let x = 50;
+    let y = 250;
+    //counting variable 20x
+    for(let i = 0; i < 20; i++){
+      noStroke();  
+      fill(255);
+      rectMode(CENTER);
+      rect(x,y,2,50);
+      x = x + 5;
+
+    }
+
+
 }
 
 
