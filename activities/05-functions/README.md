547b1ff32bbc04c837a7fdefb434bd0ec41ae4b5
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-10-02 13:35:13 -0400

    Started 5.6 Keyboard Input
    started learning 5.6 - keyboard inputs

--- activities/05-functions/README.md
+++ activities/05-functions/README.md
@@ -5,3 +5,4 @@ Learning functions with parameters 5.2
 Learning fuctions with return values 5.3
 Learning text 5.4
 Learning states 5.5
+Learning keyboard input 5.6
--- activities/05-functions/js/script.js
+++ activities/05-functions/js/script.js
@@ -1,22 +1,11 @@
 /**
- * Learning States
+ * Keyboard Input
  * Taylor McArthur
  * 
- * States 5.5
+ * States 5.6
  */
 
 "use strict";
-
-let circle = {
-    x: 0,
-    y: 250,
-    size: 100,
-    vx: 0,
-    vy: 0,
-    speed: 2
-};
-
-let state = `title`; // Possible titles are: title, animation, ending
 
 //Preload 
 function preload() {
@@ -25,55 +14,14 @@ function preload() {
 //Set up
 function setup() {
     createCanvas(500,500);
-    circle.vx = circle.speed;
-    textSize(32);
-    textAlign(CENTER,CENTER);
+
 }
 
 //Displaying the text 
 function draw() {
     background(0);
 
-    if (state === `title`){
-        title();
-    }
-
-    else if (state === `animation`) {
-        animation();
-    }
-
-    else if (state === `ending`) {
-        ending();
-    }
 }
 
-function title(){
-    //Title
-    fill(255);
-    text(`Life.`, width/2,height/2);
-}
 
-function animation(){
-    //Animation
-    circle.x = circle.x + circle.vx;
-    circle.y = circle.y + circle.vy;
 
-    if (circle.x > width){
-        state = `ending`;
-    }
-
-    ellipse(circle.x,circle.y,circle.size);
-}
-
-function ending(){
-    //Ending
-    fill(127);
-    text(`It's all over.`, width/2,height/2);
-}
-
-function keyPressed(){
-
-    if (state === `title`) {
-    state = `animation`;
-    }
-}
