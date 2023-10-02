3133224f50747bb152b940bd5dd6a43f83c2b3aa
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-10-02 13:31:40 -0400

    States 5.5
    finished up learning 5.5

--- activities/05-functions/js/script.js
+++ activities/05-functions/js/script.js
@@ -16,30 +16,64 @@ let circle = {
     speed: 2
 };
 
+let state = `title`; // Possible titles are: title, animation, ending
 
 //Preload 
 function preload() {
-
 }
-
 
 //Set up
 function setup() {
     createCanvas(500,500);
     circle.vx = circle.speed;
-
+    textSize(32);
+    textAlign(CENTER,CENTER);
 }
-
 
 //Displaying the text 
 function draw() {
     background(0);
 
+    if (state === `title`){
+        title();
+    }
+
+    else if (state === `animation`) {
+        animation();
+    }
+
+    else if (state === `ending`) {
+        ending();
+    }
+}
+
+function title(){
+    //Title
+    fill(255);
+    text(`Life.`, width/2,height/2);
+}
+
+function animation(){
+    //Animation
     circle.x = circle.x + circle.vx;
     circle.y = circle.y + circle.vy;
 
-    ellipse(circle.x,circle.y,circle.size);
+    if (circle.x > width){
+        state = `ending`;
+    }
 
-   
+    ellipse(circle.x,circle.y,circle.size);
 }
 
+function ending(){
+    //Ending
+    fill(127);
+    text(`It's all over.`, width/2,height/2);
+}
+
+function keyPressed(){
+
+    if (state === `title`) {
+    state = `animation`;
+    }
+}
