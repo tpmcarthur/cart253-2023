4329143973408ba0fd172613d9cb8406bd93f942
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-10-02 16:29:49 -0400

    Completed 5.7 automated movements

--- activities/05-functions/js/script.js
+++ activities/05-functions/js/script.js
@@ -7,6 +7,15 @@
 
 "use strict";
 
+let circle = {
+    x: 250,
+    y: 250,
+    size: 100,
+    vx: 0,
+    vy: 0,
+    speed: 2
+}
+
 //Preload 
 function preload() {
 }
@@ -16,11 +25,32 @@ function setup() {
     createCanvas(500,500);
 }
 
-//functions written below/after handle
 function draw() {
-    background(bg);
+    background(0);
 
-   
+    let dx = circle.x - mouseX;
+    let dy = circle.y - mouseY;
+
+    if(dx < 0) {
+        circle.vx = circle.speed;
+    }
+
+    else if (dx > 0) {
+        circle.vx = -circle.speed;
+    }
+
+    if (dy < 0) {
+        circle.vy = circle.speed;
+    }
+
+    else if (dy > 0){
+        circle.vy = -circle.speed;
+    }
+
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+
+    ellipse(circle.x,circle.y,circle.size);
 }
 
 
