0b3773ce1309e9c22f74bfc919747b29a9627091
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-10-02 14:54:49 -0400

    Update script.js

--- activities/05-functions/js/script.js
+++ activities/05-functions/js/script.js
@@ -22,27 +22,9 @@ function setup() {
 function draw() {
     background(bg);
 
-    textAlign(CENTER,CENTER);
-    textSize(64);
-    fill(255);
-    text(keyCode,width/2,height/2);
-
 }
 
-//Display/change colour when key is pressed
-function keyPressed(){
-    if (keyCode === UP_ARROW){
-        bg = bg + 10;
-        bg = constrain(bg,0,255);
-    }
 
-    //down key
-    else if (keyCode === DOWN_ARROW){
-        bg = bg - 10;
-        bg = constrain(bg,0,255);
-    }
-
-}
 
 
 
