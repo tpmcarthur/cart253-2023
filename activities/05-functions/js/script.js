6eb3b743eb0e17ccb46eddf99ff66ff35112b4d6
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-10-02 15:38:26 -0400

    Completed Functions 5.6 Keyboard inputs
    Completed 5.6, got the circle to move around the canvas with the arrows keyboard

--- activities/05-functions/js/script.js
+++ activities/05-functions/js/script.js
@@ -9,6 +9,15 @@
 
 let bg = 0;
 
+let circle = {
+    x: 250,
+    y: 250,
+    size: 100,
+    vx: 0,
+    vy: 0,
+    speed: 6
+};
+
 //Preload 
 function preload() {
 }
@@ -16,11 +25,53 @@ function preload() {
 //Set up
 function setup() {
     createCanvas(500,500);
+}
+
+//functions written below/after handle
+function draw() {
+    background(bg);
+
+    handleInput();
+    move();
+    display();   
+}
+
+function handleInput(){
+    if(keyIsDown(LEFT_ARROW)){
+        circle.vx =-circle.speed;
+    }
+
+    else if (keyIsDown(RIGHT_ARROW)){
+        circle.vx = circle.speed;
+    }
+
+    else {
+        circle.vx = 0;
+    }
+
+    if (keyIsDown(UP_ARROW)){
+        circle.vy =-circle.speed;
+    }
+
+    else if (keyIsDown(DOWN_ARROW)){
+        circle.vy = circle.speed;
+    }
+
+    else {
+        circle.vy = 0;
+    }
 
 }
 
-function draw() {
-    background(bg);
+//Move the circle on the screen
+function move(){
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+}
+
+//Draw the circle on the screen
+function display(){
+    ellipse(circle.x,circle.y,circle.size);
 
 }
 
