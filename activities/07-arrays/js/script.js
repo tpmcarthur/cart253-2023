df604e6c3d23b1dbac40be8f999b476131942966
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-11-02 17:05:40 -0400

    Trying to catch up on what I missed
    started the eat em up

---
+++ activities/07-arrays/js/script.js
@@ -0,0 +1,25 @@
+/**
+ * Eat 'em up 
+ * Taylor McArthur
+ * 
+ * 
+ */
+
+"use strict";
+
+//PreLoad
+function preload() {
+
+}
+
+
+//Setup
+function setup() {
+
+}
+
+
+//Draw
+function draw() {
+
+}
