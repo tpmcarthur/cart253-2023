65c479f5c56c52e064dcdc5670e5d9ec306fe370
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-11-03 10:44:44 -0400

    Starting 7.3 Nature of arrays
    starting the 7.3 section nature of arrays

--- activities/07-arrays/README.md
+++ activities/07-arrays/README.md
@@ -2,3 +2,4 @@
 
 7.1 Eat em up - exercise completed
 7.2 Introducing Arrays - exercise completed
+7.3 The Nature of Arrays - started exercise
--- activities/07-arrays/js/script.js
+++ activities/07-arrays/js/script.js
@@ -1,16 +1,13 @@
 /**
  * 7.1 Eat em up - done
- * 7.2 Introducing Arrays
+ * 7.2 Introducing Arrays - done
+ * 7.3 The Nature of Arrays - started
  * Taylor McArthur
  * 
  * 
  */
 
 "use strict";
-
-let school = []; //Create an empty array and assign it to the school variable
-let schoolSize = 4;
-
 
 //PreLoad
 function preload() {
@@ -19,73 +16,17 @@ function preload() {
 
 //Setup
 function setup() {
-    createCanvas(600, 600);
 
-
-    for (let i = 0; i < schoolSize; i++) {
-        //create a fish
-        let fish = createFish(random(0, width), random(0, height));
-        //add the fish to our array
-        school.push(fish);
-    }
-}
-
-//createFish(x,y)
-//Creates a new javascript object describing a fish and returns it 
-function createFish(x, y) {
-    let fish = {
-        x: x,
-        y: y,
-        size: 50,
-        vx: 0,
-        vy: 0,
-        speed: 2
-    };
-    return fish;
 }
 
 //Draw
 function draw() {
-    background(0);
 
-    for (let i = 0; i < school.length; i++) {
-        moveFish(school[i]);
-        displayFish(school[i]);
-    }
 }
 
-// moveFish(fish)
-// Chooses wether the provided fish changes direction and moves it
-function moveFish(fish) {
-    //Choose wether to change directions
-    let change = random(0, 1);
-    if (change < 0.05) {
-        fish.vx = random(-fish.speed, fish.speed);
-        fish.vy = random(-fish.speed, fish.speed);
-    }
 
-    //Move the fish
-    fish.x = fish.x + fish.vx;
-    fish.y = fish.y + fish.vy;
 
-    //Constrain the fish to the canvas
-    fish.x = constrain(fish.x, 0, width);
-    fish.y = constrain(fish.y, 0, height);
-}
 
-// displayFish(fish)
-function displayFish(fish) {
-    push();
-    fill(200, 100, 100);
-    noStroke();
-    ellipse(fish.x, fish.y, fish.size);
-    pop();
-}
-
-function mousePressed() {
-    let fish = createFish(mouseX, mouseY); // create a fish at the mouse position
-    school.push(fish); // add the fish to our array and will be moved and drawn with all the other in the for loop
-}
 
 
 
