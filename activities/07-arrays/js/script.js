fa6a90ed2a9fb2c0f140adbfb8e3edfff96aeccf
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-11-04 16:53:32 -0400

    Finished the final version of the 7.3 activities

--- activities/07-arrays/js/script.js
+++ activities/07-arrays/js/script.js
@@ -83,13 +83,37 @@ function displayFish(fish) {
     pop();
 }
 
-function mousePressed() {
-    let fish = createFish(mouseX, mouseY); // Create a fish at the mouse position
-    school.push(fish); // Add the fish to our array
-    // Now the school array has our new fish and it will be moved and drawn
-    // with all the others in the for loop!
-}
+// function mousePressed() {
+//     let fish = createFish(mouseX, mouseY); // Create a fish at the mouse position
+//     school.push(fish); // Add the fish to our array
+//     // Now the school array has our new fish and it will be moved and drawn
+//     // with all the others in the for loop!
+// }
 
+// mousePressed(), checks whether a fish in the school was clicked
+// and removes it 
+
+function mousePressed() {
+    // Use a for loop to examine every fish in the school one by one
+    for (let i = 0; i < school.length; i++) {
+        // store the current fish in the fish variable
+        let fish = school[i];
+        // calculate the distance between the mouse position and the fish 
+        let d = dist(mouseX, mouseY, fish.x, fish.y);
+        // If the distance means the mouse was clicked inside the fish 
+        if (d < fish.size / 2) {
+            // Remove the fish using the splice() function which takes two arguments
+            // - The index to start remove elements from (i in our case)
+            // - The number of elements to remove from that position (just one for us)
+            school.splice(i, 1);
+            // Now that we've found our fish to remove, we don't want to continue
+            // going through the loop, so we end it prematurely with break
+            // This forces the for-loop to stop immediately
+            break;
+
+        }
+    }
+}
 
 
 
