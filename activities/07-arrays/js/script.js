da1dd25e61f664550c2a596d71906f09a6dc031a
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-11-04 16:47:28 -0400

    Got file back to fish, doing the last section TMI? Removing fish

--- activities/07-arrays/js/script.js
+++ activities/07-arrays/js/script.js
@@ -8,43 +8,91 @@
  */
 
 "use strict";
-// An array to store our images
-let images = [];
-
-// A variable storing the number of images to load
-let numImages = 10;
-
-// A variable to store the image we want to display
-let displayImage;
+let school = [];
+let schoolSize = 4;
 
 
-// preload() loads 10 images
+// preload
 function preload() {
-    // Use a for loop to count from 0 up to 9 
-    for (let i = 0; i < numImages; i++) {
-        // load the image into a variable
-        // note that we use i to specify the number in the filename
-        // note how nice this is with a template literal string
-        let loadedImage = loadImage(`assets/images/clown-${i}.png`);
-        // add the loaded image into the images array 
-        images.push(loadedImage);
+
+}
+
+// setup
+function setup() {
+    createCanvas(600, 600);
+
+    for (let i = 0; i < schoolSize; i++) {
+        // Create a fish
+        let fish = createFish(random(0, width), random(0, height));
+        // Add the fish to our array
+        school.push(fish);
     }
 }
 
-// setup() selects the image to display randomly
-function setup() {
-    createCanvas(600, 600);
-    // Choose an image to display randomly from the array
-    displayImage = random(images);
+// createFish(x,y)
+// Creates a new JavaScript Object describing a fish and returns it
+function createFish(x, y) {
+    let fish = {
+        x: x,
+        y: y,
+        size: 50,
+        vx: 0,
+        vy: 0,
+        speed: 2
+    };
+    return fish;
+
 }
 
-// draw() displays the randomly chosen image
+// draw
 function draw() {
     background(0);
-    // Display the randomly selected image
-    imageMode(CENTER);
-    image(displayImage, width / 2, height / 2);
+
+    for (let i = 0; i < school.length; i++) {
+        moveFish(school[i]);
+        displayFish(school[i]);
+    }
 }
+
+// moveFish(fish)
+// Chooses whether the provided fish changes direction and moves it
+function moveFish(fish) {
+    // Choose whether to change direction
+    let change = random(0, 1);
+    if (change < 0.05) {
+        fish.vx = random(-fish.speed, fish.speed);
+        fish.vy = random(-fish.speed, fish.speed);
+    }
+
+    // Move the fish
+    fish.x = fish.x + fish.vx;
+    fish.y = fish.y + fish.vy;
+
+    // Constrain the fish to the canvas
+    fish.x = constrain(fish.x, 0, width);
+    fish.y = constrain(fish.y, 0, height);
+}
+
+// displayFish(fish)
+// Displays the provided fish on the canvas
+function displayFish(fish) {
+    push();
+    fill(200, 100, 100);
+    noStroke();
+    ellipse(fish.x, fish.y, fish.size);
+    pop();
+}
+
+function mousePressed() {
+    let fish = createFish(mouseX, mouseY); // Create a fish at the mouse position
+    school.push(fish); // Add the fish to our array
+    // Now the school array has our new fish and it will be moved and drawn
+    // with all the others in the for loop!
+}
+
+
+
+
 
 
 
