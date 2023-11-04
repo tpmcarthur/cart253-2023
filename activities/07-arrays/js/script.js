c5995a460f4bd2d8220e1c83fc0868f3b5797293
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-11-04 14:49:39 -0400

    started the limited trail section

--- activities/07-arrays/js/script.js
+++ activities/07-arrays/js/script.js
@@ -9,13 +9,6 @@
 
 "use strict";
 
-let circle = {
-    x: 0,
-    y: 0,
-    size: 100,
-    trail: [] // creating an empty trail array as a property of the circle
-}
-
 //PreLoad
 function preload() {
 
@@ -23,39 +16,13 @@ function preload() {
 
 //Setup
 function setup() {
-    createCanvas(600, 600);
+
 
 }
 
 //Draw
 function draw() {
-    background(0);
 
-    // Use a for loop to go through each element in the circles trail array in order
-    for (let i = 0; i < circle.trail.length; i++) {
-        // Get element at the index indicated by i (0, then 1, then 2 etc.)
-        let element = circle.trail[i];
-        // drae an ellipse the same size as the circle at that position 
-        ellipse(element.x, element.y, circle.size);
-    }
-
-    // Move the circle to the mouse position
-    circle.x = mouseX;
-    circle.y = mouseY;
-
-    // draw the circle
-    ellipse(circle.x, circle.y, circle.size);
-
-    // Create a new position object that stores where the circle is now 
-    // which we can add to the trail to trace the path of the circle 
-
-    let newTrailPosition = {
-        x: circle.x,
-        y: circle.y
-    };
-
-    //add the position to the circles trail array 
-    circle.trail.push(newTrailPosition);
 
 }
 
