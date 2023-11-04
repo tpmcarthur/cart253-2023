6a729682a907c4455d69394540888283f5e2d1df
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-11-04 16:05:28 -0400

    Update script.js

--- activities/07-arrays/js/script.js
+++ activities/07-arrays/js/script.js
@@ -8,14 +8,13 @@
  */
 
 "use strict";
-// an array to store our images
+// An array to store our images
 let images = [];
-
-// a variable to store the image we want to display 
+// A variable to store the image we want to display
 let displayImage;
 
 
-//PreLoad() loads 10 images
+// preload() loads 10 images
 function preload() {
     images[0] = loadImage(`assets/images/clown-0.png`);
     images[1] = loadImage(`assets/images/clown-1.png`);
@@ -29,21 +28,20 @@ function preload() {
     images[9] = loadImage(`assets/images/clown-9.png`);
 }
 
-//setup() selects the image to display randomly
+// setup() selects the image to display randomly
 function setup() {
     createCanvas(600, 600);
-    // choose an image to display randomly from the array 
+    // Choose an image to display randomly from the array
     displayImage = random(images);
 }
 
-//draw() displays the randomly chosen image
+// draw() displays the randomly chosen image
 function draw() {
     background(0);
-    // display the randomly selected image
+    // Display the randomly selected image
     imageMode(CENTER);
     image(displayImage, width / 2, height / 2);
 }
-
 
 
 
