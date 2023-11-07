8ea9c6b67f0bc61f22f1b2883c49ab3a1dfa6be7
tpmcarthur <76931896+tpmcarthur@users.noreply.github.com>
2023-11-07 13:59:28 -0500

    Finished 8.1 - Introducing OOP

--- activities/08-object oriented programming/js/Flower.js
+++ activities/08-object oriented programming/js/Flower.js
@@ -1,12 +1,11 @@
-"use strict";
 
 class Flower {
 
-    // The constructor() sets up a flower's properties
+    // The constructor() sets up a this's properties
     constructor() {
         // Position and size information
-        this.x = x;
-        this.y = y;
+        this.x = random(0, width);
+        this.y = random(0, height);
         this.size = 50;
         this.stemLength = 75;
         this.stemThickness = 10;
@@ -29,16 +28,13 @@ class Flower {
         };
     }
 
-    // display()
-    // Displays the flower on the canvas
     display() {
         push();
-        // Set the stroke weight for the petals and the stem
-        strokeWeight(this.stemThickness);
         // Draw a line for the stem
+        strokeWeight(this.stemThickness);
         stroke(this.stemColor.r, this.stemColor.g, this.stemColor.b);
         line(this.x, this.y, this.x, this.y + this.stemLength);
-        // Draw a circle with a heavy outline for the flower
+        // Draw a circle with a heavy outline for the this
         strokeWeight(this.petalThickness);
         fill(this.centreColor.r, this.centreColor.g, this.centreColor.b);
         stroke(this.petalColor.r, this.petalColor.g, this.petalColor.b);
--- activities/08-object oriented programming/js/script.js
+++ activities/08-object oriented programming/js/script.js
@@ -23,7 +23,6 @@ function preload() {
 
 }
 
-
 // setup() creates the canvas and the flowers in the garden
 function setup() {
     createCanvas(600, 600);
@@ -34,59 +33,10 @@ function setup() {
         let flower = new Flower();
         // Add the flower to the array of flowers
         garden.flowers.push(flower);
+        console.log("garden flowers array");
     }
 }
 
-// createFlower()
-// Creates a new JavaScript Object describing a flower and returns it
-function createFlower() {
-    // Create our object
-    let flower = {
-        // Position and size information
-        x: random(0, width),
-        y: random(0, height),
-        size: 50,
-        stemLength: 75,
-        stemThickness: 10,
-        petalThickness: 10,
-        // Color information
-        stemColor: {
-            r: 50,
-            g: 150,
-            b: 50
-        },
-        petalColor: {
-            r: 200,
-            g: 50,
-            b: 50
-        },
-        centreColor: {
-            r: 50,
-            g: 0,
-            b: 0
-        }
-    };
-    return flower;
-}
-
-// displayFlower(flower)
-// Displays the provided flower on the canvas
-function displayFlower(flower) {
-    push();
-    // Draw a line for the stem
-    strokeWeight(flower.stemThickness);
-    stroke(flower.stemColor.r, flower.stemColor.g, flower.stemColor.b);
-    line(flower.x, flower.y, flower.x, flower.y + flower.stemLength);
-    // Draw a circle with a heavy outline for the flower
-    strokeWeight(flower.petalThickness);
-    fill(flower.centreColor.r, flower.centreColor.g, flower.centreColor.b);
-    stroke(flower.petalColor.r, flower.petalColor.g, flower.petalColor.b);
-    ellipse(flower.x, flower.y, flower.size);
-    pop();
-}
-
-// draw()
-// Displays our flowers
 function draw() {
     // Display the grass
     background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
@@ -94,6 +44,9 @@ function draw() {
     // Loop through all the flowers in the array and display them
     for (let i = 0; i < garden.flowers.length; i++) {
         let flower = garden.flowers[i];
-        flower.display(); // NEW! Call the display() method for this flower
+        flower.display();
+        console.log("garden flowers should be here!");
     }
 }
+
+
